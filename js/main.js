5283a3bdf53e946f50fba9307272460386db6950
MohammadRadja <you@example.com>
2024-05-20 16:36:48 +0700

    revisi

--- js/main.js
+++ js/main.js
@@ -66,31 +66,20 @@
   };
   carousel();
 
-  $("nav .dropdown").hover(
+  $("nav .dropdown-menu").hover(
     function () {
       var $this = $(this);
-      // 	 timer;
-      // clearTimeout(timer);
       $this.addClass("show");
       $this.find("> a").attr("aria-expanded", true);
-      // $this.find('.dropdown-menu').addClass('animated-fast fadeInUp show');
       $this.find(".dropdown-menu").addClass("show");
     },
     function () {
       var $this = $(this);
-      // timer;
-      // timer = setTimeout(function(){
       $this.removeClass("show");
       $this.find("> a").attr("aria-expanded", false);
-      // $this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
       $this.find(".dropdown-menu").removeClass("show");
-      // }, 100);
     }
   );
-
-  $("#dropdown04").on("show.bs.dropdown", function () {
-    console.log("show");
-  });
 
   // scroll
   var scrollWindow = function () {
